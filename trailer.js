419c9f0c7f26d75035c5a0129c0b69cbcd6cbb40
Floris Tabak <floristabak@Floriss-MacBook-Air.local>
2025-03-03 20:16:19 +0100

    i tried

--- trailer.js
+++ trailer.js
@@ -8,18 +8,25 @@ function gettrailer(){
     }
 }
 
-
-window.onmousemove = e => {
+const animatetrailer = (e, interacting) => {
     const x = e.clientX - trailer.offsetWidth / 2,
         y = e.clientY - trailer.offsetHeight / 2;
 
     const keyframes = {
-        transform: `translate(${x}px, ${y}px)`
+        transform: `translate(${x}px, ${y}px)
+        scale(${interacting ? 2 : 1})`
     };
 
     trailer.animate(keyframes, {
         duration: 800,
         fill: "forwards"
     });
+}
+
+window.onmousemove = e => {
+    const interactable = e.target.closest(".interactable"),
+        interacting = interactable !== null;
+
+    animatetrailer(e, interacting);
 }
 gettrailer();
